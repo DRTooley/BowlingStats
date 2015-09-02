4d61b6bb2dd3b33d783b44ca3d0ccf01db27686e
David Tooley <David.Tooley@live.com>
2015-09-02 18:31:54 -0500

    Slight Update

--- CalculateStats.py
+++ CalculateStats.py
@@ -1,34 +1,48 @@
 import sqlite3
 import datetime
-if __name__ == "__main__":
-    dbcon = sqlite3.connect("BowlingDatabase.db")
-    cursor = dbcon.cursor()
 
+import matplotlib.pyplot as pyplot
 
-    
-    cursor.execute("SELECT * FROM stats")
-    BowlingInformation = [row for row in cursor]
+class BowlingEntry():
+    def __init__(self, EntryData):
+        self.DateBowled = EntryData[0]
+        self.Score = EntryData[1]
+        self.League = EntryData[2]
+
+def CalculateLongestHotStreak(BowlingInformation, HotNumber=200):
     longestStreak = 0
     currentStreak = 0
     tmpStart = datetime.datetime.now()
     streakStart = datetime.datetime.now()
     streakEnd = datetime.datetime.now()
-    for score in BowlingInformation:
+    for entry in BowlingInformation:
         if currentStreak == 0:
-            tmpStart = datetime.datetime.strptime(score[0], "%Y-%m-%d")
-        if score[1] >= 200:
+            tmpStart = datetime.datetime.strptime(entry.DateBowled, "%Y-%m-%d")
+        if entry.Score >= HotNumber:
             currentStreak += 1
         elif currentStreak > longestStreak:
             streakStart = tmpStart
-            streakEnd = datetime.datetime.strptime(score[0], "%Y-%m-%d")
+            streakEnd = datetime.datetime.strptime(entry.DateBowled, "%Y-%m-%d")
             longestStreak = currentStreak
             currentStreak = 0
         else:
             currentStreak = 0
 
-    print("Your longest 200 Streak is " + str(longestStreak) + " games!")
+    print("Your longest "+ str(HotNumber) +"+ streak is " + str(longestStreak) + " games!")
     print("Starting on " + streakStart.strftime('%m/%d/%Y') + " and ending on " + streakEnd.strftime('%m/%d/%Y'))
+    print('')
 
+def GraphYearlyHighScores(BowlingInformation):
+    pass
 
+if __name__ == "__main__":
+    dbcon = sqlite3.connect("BowlingDatabase.db")
+    cursor = dbcon.cursor()
+    cursor.execute("SELECT * FROM stats")
+    BowlingInformation = [BowlingEntry(row) for row in cursor]
+
+    CalculateLongestHotStreak(BowlingInformation)
+
+    GraphYearlyHighScores(BowlingInformation)
 
     dbcon.close()
